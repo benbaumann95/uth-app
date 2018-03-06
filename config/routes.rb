2b7e6eeebdb8e031b24a3517dbd1f99297dd8b26
Cheryl Chua <cherylsychua@outlook.com>
2018-03-06 17:05:55 +0000

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#dashboard'
 
   resources :events, only: [:index, :show, :create, :new]
-    resources :tickets do
+    resources :tickets , only: [:index, :show, :create, :destroy] do
       resources :bookings, only: [:create]
     end
 end
