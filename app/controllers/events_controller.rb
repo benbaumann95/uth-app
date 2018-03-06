e0b0dac071036feac4338afe24bb0646364f5f22
Cheryl Chua <cherylsychua@outlook.com>
2018-03-06 11:49:46 +0000

    list a new event and ticket

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,4 +4,32 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = Event.new
+    @ticket = Ticket.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.save
+
+    @ticket = Ticket.new(ticket_params)
+    @ticket.event = @event
+    @ticket.user = current_user
+    @ticket.display_flag = @ticket.sold = false
+    @ticket.save
+
+    redirect_to root_path
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :quantity, :location, :date, :start_time, :end_time, :category)
+  end
+
+  def ticket_params
+    params[:event].require(:ticket).permit(:photo, :event_id, :display_flag, :user_id, :price, :description, :sold)
+  end
 end
--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,25 +1,2 @@
 class TicketsController < ApplicationController
-  def new
-    @event = Event.new
-    @ticket = Ticket.new
-  end
-
-  def create
-    @event = Event.new(event_params)
-    @event.create
-    @ticket = Ticket.new(ticket_params)
-    @ticket.event = @event
-    @ticket.user = current_user
-    @ticket.create
-  end
-
-  private
-
-  def ticket_params
-    params.require(:ticket).permit(:photo, :event_id, :display_flag, :user_id, :price, :description, :sold)
-  end
-
-  def event_params
-    params.require(:event).permit(:name, :quantity, :location, :date, :start_time, :end_time, :category)
-  end
 end
