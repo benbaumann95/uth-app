a95f4f916c9caed6c57308aae178cef736b7cd5d
Cheryl Chua <cherylsychua@outlook.com>
2018-03-08 12:01:17 +0000

    added home page and controllers and rerouted stuff

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,23 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :about
+  skip_before_action :authenticate_user!, only: [:about, :home]
 
   def about
   end
+
   def dashboard
     @tickets = current_user.tickets
     @bookings = current_user.bookings
     @number_listings = current_user.tickets.all.count
     @number_purchases = current_user.bookings.all.count
   end
+
   def ticket_file
   end
+
   def profile
+  end
+
+  def home
+    @events = policy_scope(Event)
   end
 end
