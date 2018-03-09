fb8de426d941d2c83e71ec71c43b22c697201ad3
Ben Baumann <benbaumann95@hotmail.co.uk>
2018-03-09 11:03:54 +0000

    heart animation and watchlist logic

---
+++ app/javascript/packs/event-index.js
@@ -0,0 +1,17 @@
+$(".heart").on('click', function(){
+  $(this).toggleClass('is_animating');
+});
+$('.heart').on('animationend', function(){
+  $(this).toggleClass('is_animating');
+});
+
+const heart = document.querySelectorAll(".heart")
+console.log(heart[0]);
+heart.forEach(heart_element => {
+  heart_element.addEventListener("click", () => {
+    if (heart_element.hasAttribute("style", "background-position:right;"))
+      heart_element.removeAttribute ("style", "background-position:right;");
+    else
+    heart_element.setAttribute("style", "background-position:right;");
+  });
+});
