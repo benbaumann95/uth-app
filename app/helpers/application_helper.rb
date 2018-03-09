b86605c403421ce683a5ede2d96fd3abd2eeee58
Amresh Selvaskandan <amresh911@gmail.com>
2018-03-09 14:05:49 +0000

    pictures

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,14 +17,22 @@ module ApplicationHelper
   end
 
 
-  def category_picture(name)
+  def city_picture(name)
+    case name
+      when "London" then 'club-pic'
+      when "Bristol" then 'club-rave-pic'
+      when nil then 'festival-sitting-down'
+    end
+  end
+
+   def category_picture(name)
     case name
       when "Club night" then 'club-pic'
       when "Concert" then 'club-rave-pic'
       when "Festival" then 'festival-sitting-down'
       when "Travel" then 'travel-pic'
       when "Sport" then 'sports-rugby'
-      when "Society" then 'electronics'
+      when "Society" then 'society'
       when "Socials" then 'prosecco-ball'
     end
   end
