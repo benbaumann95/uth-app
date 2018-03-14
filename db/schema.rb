0eccaf88d375666053347e4916978aa7e4f1ba3d
Cheryl Chua <cherylsychua@outlook.com>
2018-03-14 17:46:19 +0000

    added money rails gem and added pricing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312155030) do
+ActiveRecord::Schema.define(version: 20180314162746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20180312155030) do
     t.bigint "event_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "pricing_cents", default: 0, null: false
     t.index ["event_id"], name: "index_tickets_on_event_id"
     t.index ["user_id"], name: "index_tickets_on_user_id"
   end
