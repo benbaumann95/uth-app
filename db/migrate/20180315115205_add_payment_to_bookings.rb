ff34d67f77691c48b407ae7c60531800c910c48e
Cheryl Chua <cherylsychua@outlook.com>
2018-03-15 15:23:13 +0000

    added columns to booking

--- db/migrate/20180315115205_add_payment_to_bookings.rb
+++ db/migrate/20180315115205_add_payment_to_bookings.rb
@@ -1,4 +1,6 @@
 class AddPaymentToBookings < ActiveRecord::Migration[5.1]
   def change
+    add_column :bookings, :payment, :jsonb
+    add_monetize :bookings, :amount, currency: { present: false }
   end
 end
